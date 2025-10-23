62b44eac6fb113a8272833e46f2e9dfd171a713b
Premanshu Bhagat <premtsapj@gmail.com>
2025-10-23 15:11:03 -0400

    added definition for the show_art function

--- helpers.py
+++ helpers.py
@@ -1,6 +1,9 @@
 #pylint: disable=C0114
 #pylint: disable=C0305
 
+import time
+import sys
+import os
 import pyttsx3
 from art import bat3
 
@@ -17,5 +20,16 @@ def show_art():
     '''
     print punchlines and show art at end.
     '''
-    pass
+    os.system("cls")
+    print("Sequence complete...\n")
+    time.sleep(1)
+    print("I am Vengeance")
+    time.sleep(1)
+    print("I am the Night")
+    time.sleep(1)
+    print("I am Batman")
+    time.sleep(1)
+    print(bat3)
+    time.sleep(4)
+    sys.exit()
 
