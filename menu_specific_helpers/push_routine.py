cc0e2d4e8323a528418adbf79c62a594284760d8
Premanshu Bhagat <premtsapj@gmail.com>
2025-12-03 20:48:38 -0500

    created a dedicated folder for menu helpers.

---
+++ menu_specific_helpers/push_routine.py
@@ -0,0 +1,24 @@
+#pylint: skip-file
+#type: ignore
+
+import subprocess
+import os
+
+def menu_push_routine():
+
+    branch = subprocess.check_output(
+        ["git", "branch", "--show-current"], text=True
+    ).strip()
+
+    res = subprocess.run(
+        ["git", "log", f"origin/{branch}..HEAD", "--oneline"], capture_output=True, text=True
+    )
+
+    if res.stdout:
+        print("Commit History:")
+        print(res.stdout)
+        remote_check = os.system("git remote -v")
+        if remote_check == 0: 
+            os.system("git push")
+        else:
+            print("Remote does not exist.")
