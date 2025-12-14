aeaaca9bf43df06569113b72440b8e7f4fe4b23d
Premanshu Bhagat <premtsapj@gmail.com>
2025-12-14 11:57:30 -0500

    modularized test sandbox

--- testing.py
+++ testing.py
@@ -2,49 +2,51 @@
 
 # isolated testing env for functions being worked on.
 
+def menu_test():
+    op_dict = {
+            0 : "default routine",
+            1 : "status check",
+            2 : "branch ops",
+            3 : "stage changes",
+            4 : "commit changes",
+            5 : "push changes",
+        }
 
-op_dict = {
-        0 : "default routine",
-        1 : "status check",
-        2 : "branch ops",
-        3 : "stage changes",
-        4 : "commit changes",
-        5 : "push changes",
-    }
+    op_msgs = [
+    "executing default routine",
+    "fetching current branch status",
+    "loading bracnch ops selection menu",
+    "proceeding with staging operation",
+    "proceeding with commit sequence for staged changes",
+    "pushing commited changes to remote"]
 
-op_msgs = [
-"executing default routine",
-"fetching current branch status",
-"loading bracnch ops selection menu",
-"proceeding with staging operation",
-"proceeding with commit sequence for staged changes",
-"pushing commited changes to remote"]
-
-print("Auto Git Sequence Menu:\n")
-print("*"*70)
-print("id |     operation")
-print("----------------------")
-for key,val in op_dict.items():
-    print(f"{key}  | {val}")
-print("*"*70)
-print()
-print("ALERT: invalid input will result in fallback to default routine.\n")
-try:
-    menu_selection = int(input("Enter op id: "))
-except ValueError:
+    print("Auto Git Sequence Menu:\n")
+    print("*"*70)
+    print("id |     operation")
+    print("----------------------")
+    for key,val in op_dict.items():
+        print(f"{key}  | {val}")
+    print("*"*70)
     print()
-    print("*"*70)
-    print("invalid input...")
-    print("fallback to default routine.")
-    menu_selection = 0
-    print()
-if menu_selection not in op_dict.keys():
-    print("*"*70)
-    print("INVALID OP ID: RANGE ERROR")
-    print("fallback to default routine")
-    print("*"*70)
-else: 
-    print(f"setting op id: {menu_selection}")
-    print(op_msgs[menu_selection])
-    print("*"*70)
+    print("ALERT: invalid input will result in fallback to default routine.\n")
+    try:
+        menu_selection = int(input("Enter op id: "))
+    except ValueError:
+        print()
+        print("*"*70)
+        print("invalid input...")
+        print("fallback to default routine.")
+        menu_selection = 0
+        print()
+    if menu_selection not in op_dict.keys():
+        print("*"*70)
+        print("INVALID OP ID: RANGE ERROR")
+        print("fallback to default routine")
+        print("*"*70)
+    else: 
+        print(f"setting op id: {menu_selection}")
+        print(op_msgs[menu_selection])
+        print("*"*70)
 
+if __name__ == "__main__":
+    menu_test()
